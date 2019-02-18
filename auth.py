c18bb933e04ff4711ae96894c290dc57f1e0886d
Hendrik Geßner <HENGE01@gmail.com>
2019-02-18 12:42:35 +0100

    Added salting to password hash
    When creating a user, a salt is created. The salt is read and used while hashing whenever a password is checked

--- auth.py
+++ auth.py
@@ -1,21 +1,35 @@
 import getpass
 import pickle
 import hashlib
+import random
+import string
 
-def pwhash(password):
+CHARS = string.ascii_letters + string.digits + string.punctuation
+
+def pwhash(password, salt):
     m = hashlib.sha512()
+    m.update(salt.encode('utf-8'))
     m.update(password.encode('utf-8'))
     return m.digest()
 
+def create_salt():
+    salt_chars = random.choices(CHARS, k=10)
+    salt = ''.join(salt_chars)
+    return salt
+
 def get_credentials():
     username = input("Enter username:")
-    password = pwhash(getpass.getpass("Enter password:"))
+    password = getpass.getpass("Enter password:")
     return (username, password)
 
 def authenticate(username, password, pwdb):
     status = False
     if username in pwdb:
-        if password == pwdb[username]:
+        # get hashed password and salt
+        hashed_password = pwdb[username]['password']
+        user_salt = pwdb[username]['salt']
+        # check given password with expected value
+        if hashed_password == pwhash(password, user_salt):
             status = True
         else:
             print('Wrong password!')
@@ -25,7 +39,13 @@ def authenticate(username, password, pwdb):
     return status
 
 def add_user(username, password, pwdb):
-    pwdb[username] = password
+    user_salt = create_salt();
+    hashed_password = pwhash(password, user_salt)
+    
+    pwdb[username] = {
+            'salt': user_salt,
+            'password': hashed_password,
+            }
     write_pwdb(pwdb)
 
 def read_pwdb():
